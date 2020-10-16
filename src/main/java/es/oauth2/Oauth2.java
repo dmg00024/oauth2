fffbac31f236b3433a3e5300bb3cc194ad0bb570
Daniel <dmg00024@gmail.com>
2020-10-16 02:24:18 +0200

    primera ejecución

---
+++ src/main/java/es/oauth2/Oauth2.java
@@ -0,0 +1,11 @@
+package es.oauth2;
+
+public class Oauth2 
+{
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("prueba");
+	}
+
+}
---
+++ src/test/java/oauth2/TestCase.java
@@ -0,0 +1,15 @@
+package oauth2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCase 
+{
+	@Test
+	public void prueba() 
+	{
+		assertFalse(false);
+	}
+
+}
